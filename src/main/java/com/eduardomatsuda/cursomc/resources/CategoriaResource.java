35661f9f83184ec5732bb606fdfe93b9d0091b10
Eduardo Y Matsuda <eymatsuda2016@outlook.com>
2022-06-18 22:20:11 -0300

    Produto e Associação Muitos para Muitos

---
+++ src/main/java/com/eduardomatsuda/cursomc/resources/CategoriaResource.java
@@ -0,0 +1,30 @@
+package com.eduardomatsuda.cursomc.resources;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.eduardomatsuda.cursomc.domain.Categoria;
+import com.eduardomatsuda.cursomc.services.CategoriaService;
+
+@RestController
+@RequestMapping(value= "/categorias")
+public class CategoriaResource {
+	
+	@Autowired
+	private CategoriaService service;
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.GET)
+	public ResponseEntity<?> buscar(@PathVariable Integer id) {
+		Categoria obj = service.buscar(id);
+		return ResponseEntity.ok().body(obj);	
+
+	}
+
+}
